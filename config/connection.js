b04eee495b48b415eec194f16ac4fc5f042b68a3
unknown <pross2252@gmail.com>
2022-08-03 01:11:35 -0400

    deploying app

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 const connectionString =
-  process.env.MONGODB_URI
+process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetwork';
 
 connect(connectionString, {
   useNewUrlParser: true,
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-
 db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server is running on port: ${PORT}!`);
